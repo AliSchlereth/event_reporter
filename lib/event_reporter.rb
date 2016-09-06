9e2253240fa4df3af19506e54f912efef952772d
Ali Schlereth <aschlere@sbcglobal.net>
2016-09-05 21:40:35 -0600

    Functioning clean_data

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -9,19 +9,30 @@ class EventReporter
 
   def clean_data(attendees)
     cleaned_attendees = {}
-    attendees.each do |row|
+    attendees.each_with_index do |row, i|
       first_name = clean_first_name(row[:first_name])
       last_name = clean_last_name(row[:last_name])
       email = clean_to_string(row[:email_address])
-      home_phone = clean_home_phone(row[:home_phone])
+      home_phone = clean_home_phone(row[:homephone])
       street = clean_to_string(row[:street])
-      # city = row[:city].to_s
-      cleaned_attendees[row[:id]] = {"first_name" => clean_first_name ,
-                                      "last_name" => clean_last_name ,
-                                      "email"     => clean_email
-                                    }
-      # binding.pry
+      city = clean_to_string(row[:city])
+      state = clean_state(row[:state])
+      zipcode = clean_zipcode(row[:zipcode])
+      cleaned_attendees[i+1] = {"first_name" => first_name,
+                                "last_name"  => last_name,
+                                "email"      => email,
+                                "home_phone" => home_phone,
+                                "street"     => street,
+                                "city"       => city,
+                                "state"      => state,
+                                "zipcode"    => zipcode
+                                }
     end
+    cleaned_attendees
+  end
+
+  def clean_to_string(entry)
+    entry.to_s
   end
 
   def clean_first_name(first_name)
@@ -32,12 +43,16 @@ class EventReporter
     last_name.to_s.strip.downcase.capitalize
   end
 
-  def clean_to_string(entry)
-    entry.to_s
-  end
-
   def clean_home_phone(phone)
     phone.to_s.gsub(/[^0-9]/, '').rjust(10, "0")
+  end
+
+  def clean_state(state)
+    state.upcase[0..1]
+  end
+
+  def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5, "0")[0..4]
   end
 
 end
--- test/event_reporter_test.rb
+++ test/event_reporter_test.rb
@@ -12,11 +12,11 @@ class EventReporterTest < Minitest::Test
   #   assert_instance_of CSV, reporter.load("event_attendees_short.csv")
   # end
 
-  # def test_clean_data_returns_a_modified_array
-  #   reporter = EventReporter.new
-  #
-  #   assert_instance_of Array, reporter.load
-  # end
+  def test_clean_data_returns_a_hash
+    reporter = EventReporter.new
+
+    assert_instance_of Hash, reporter.load
+  end
 
   def test_first_name_returns_a_name_with_one_capital_at_the_beginning
     reporter = EventReporter.new
@@ -38,6 +38,8 @@ class EventReporterTest < Minitest::Test
     assert_equal "7384950284", reporter.clean_home_phone(7384950284)
     assert_equal "0000950284", reporter.clean_home_phone("950284")
     assert_equal "0000950284", reporter.clean_home_phone("95AA--0284")
+    assert_equal "6154385000", reporter.clean_home_phone("6154385000")
+    assert_equal "4145205000", reporter.clean_home_phone("414-520-5000")
   end
 
   def test_clean_to_string_ensures_entry_is_a_string
@@ -46,7 +48,22 @@ class EventReporterTest < Minitest::Test
     assert_equal "asch@gmail.com", reporter.clean_to_string("asch@gmail.com")
     assert_equal "124 Street Dr", reporter.clean_to_string("124 Street Dr")
     assert_equal "", reporter.clean_to_string(nil)
+  end
 
+  def test_clean_state_returns_two_capitalized_letters
+    reporter = EventReporter.new
+
+    assert_equal "CO", reporter.clean_state("co")
+    assert_equal "IL", reporter.clean_state("illinois")
+  end
+
+  def test_clean_zipcode_return_5_digits
+    reporter = EventReporter.new
+
+    assert_equal "12345", reporter.clean_zipcode("12345")
+    assert_equal "00000", reporter.clean_zipcode(nil)
+    assert_equal "00123", reporter.clean_zipcode("123")
+    assert_equal "12345", reporter.clean_zipcode("123456789")
   end
 
 
